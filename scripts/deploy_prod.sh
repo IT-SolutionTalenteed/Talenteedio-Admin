1e264dc8bbd1f319adf4a5c07f1619b335c69cd8
Happy-Franck <solofonirina35@gmail.com>
2026-03-04 15:37:13 +0300

    ci/cd admin

---
+++ scripts/deploy_prod.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm use v18.17
+
+echo "🚀 Deploy Admin..."
+
+git fetch origin
+git reset --hard origin/main
+git pull origin main
+
+echo "📦 Install deps..."
+npm install
+
+echo "🛠 Build admin..."
+npm run build
+
+echo "📂 Deploy to nginx folder..."
+
+sudo rm -rf /var/www/html/back-office/*
+sudo cp -r dist/* /var/www/html/back-office/
+
+echo "✅ Admin deployed!"
+
