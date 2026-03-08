2609f16f8075f4dbfc944160d4552e1a24759718
Happy-Franck <solofonirina35@gmail.com>
2026-03-08 14:40:36 +0300

    averina lavitra ny ci/cd

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -3,13 +3,21 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm use v18.18   # <-- changement ici
+nvm use v18.17
 
-echo "🚀 Starting deployment..."
+echo "🚀 Deploy Admin..."
 
 git fetch origin
 git reset --hard origin/main
 git pull origin main
 
+echo "📦 Install deps..."
 npm install
+
+echo "🛠 Build admin..."
 npm run build
+
+echo "📂 Deploy to nginx folder..."
+
+echo "✅ Admin deployed!"
+
