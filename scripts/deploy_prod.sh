7c9126cd9692a2eafabb0b971dcd67147e434a6a
Happy-Franck <solofonirina35@gmail.com>
2026-03-08 14:54:53 +0300

    ci/cd propre amzay

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+set -e
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source "$NVM_DIR/nvm.sh"
 
-nvm use v18.17
+nvm use 18
 
 echo "🚀 Deploy Admin..."
 
 git fetch origin
 git reset --hard origin/main
-git pull origin main
 
 echo "📦 Install deps..."
 npm install
@@ -17,7 +17,4 @@ npm install
 echo "🛠 Build admin..."
 npm run build
 
-echo "📂 Deploy to nginx folder..."
-
 echo "✅ Admin deployed!"
-
